a96e0ccec67831d6024b457c13c1398cf9772cd9
zhangpengbo <grannet@163.com>
2024-04-08 21:35:41 +0800

    Create mnist_ddp_sm.py

---
+++ mnist_ddp_sm.py
@@ -0,0 +1,72 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.distributed as dist
+from torchvison import datasets, transforms
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data import DataLoader, DistributedSampler
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(1,32,3,1)
+        self.conv2 = nn.COnv2d(32,64,3,1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216,128)
+        slef.fc2 = nn.Linear(128,10)
+
+    def forward(self,x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+def train(rank,world_size):
+    # initialize distributed training
+    dist.init_process_group("nccl",rank=rank,world_size=world_size)
+    torch.manual_seed(42)
+    torch.cuda.set_device(rank)
+
+    # load data  and save it to /data directory(where s3 mounted)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transfroms.Normalize((0.1308,),(0.3081,))
+    ]) 
+
+    dataset = datasets.MNIST('../data', train=True, download=True, transform=transform)
+    sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank)
+    train_loader = DataLoader(dataset, sampler=sampler, batch_size=64)
+
+    model = ConvNet().cuda(rank)
+    model = DDP(model, device_ids=[rank])
+    optimizer = optim.Adadelta(model.parameters(), lr=1.0)
+
+    model.train()
+    for epoch in range(1, 2):  # 训练1个epoch
+        sampler.set_epoch(epoch)
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.cuda(rank), target.cuda(rank)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % 10 == 0:
+                print(f"Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.sampler)} ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}")
+
+if __name__ == "__main__":
+    world_size = int(os.environ['WORLD_SIZE'])
+    rank = int(os.environ['RANK'])
+    train(rank, world_size)
