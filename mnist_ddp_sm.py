f5bb28155945b342928ffe91d21f98cda09ae52f
zhangpengbo <grannet@163.com>
2024-04-08 21:46:25 +0800

    Update mnist_ddp_sm.py

--- mnist_ddp_sm.py
+++ mnist_ddp_sm.py
@@ -4,21 +4,21 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.distributed as dist
-from torchvison import datasets, transforms
+from torchvision import datasets, transforms
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader, DistributedSampler
 
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(1,32,3,1)
-        self.conv2 = nn.COnv2d(32,64,3,1)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.dropout1 = nn.Dropout2d(0.25)
         self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(9216,128)
-        slef.fc2 = nn.Linear(128,10)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.conv1(x)
         x = F.relu(x)
         x = self.conv2(x)
@@ -33,19 +33,16 @@ class ConvNet(nn.Module):
         output = F.log_softmax(x, dim=1)
         return output
 
-def train(rank,world_size):
-    # initialize distributed training
-    dist.init_process_group("nccl",rank=rank,world_size=world_size)
-    torch.manual_seed(42)
+def train(rank, world_size):
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    torch.manual_seed(0)
     torch.cuda.set_device(rank)
 
-    # load data  and save it to /data directory(where s3 mounted)
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transfroms.Normalize((0.1308,),(0.3081,))
-    ]) 
-
-    dataset = datasets.MNIST('/data', train=True, download=True, transform=transform)
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    dataset = datasets.MNIST('../data', train=True, download=True, transform=transform)
     sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank)
     train_loader = DataLoader(dataset, sampler=sampler, batch_size=64)
 
